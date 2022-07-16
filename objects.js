0e1aff588dbfb7534b1436ffc3ef841f99a00dca
Martin Klein <martin.klein.austria@gmail.com>
2022-07-16 13:31:58 +0200

    Add objects.js

---
+++ objects.js
@@ -0,0 +1,63 @@
+const robot = {
+  model: 'B-4MI',
+  mobile: true,
+  greeting() {
+  	console.log(`I'm model ${this.model}, how may I be of service?`);
+  }
+}
+
+const massProdRobot = (model, mobile) => {
+  return {
+    model,
+    mobile,
+    greeting() {
+      console.log(`I'm model ${this.model}, how may I be of service?`);
+    }
+  }
+}
+
+const shinyNewRobot = massProdRobot('TrayHax', true)
+
+const chargingStation = {
+  _name: 'Electrons-R-Us',
+  _robotCapacity: 120,
+  _active: true,
+  _chargingRooms: ['Low N Slow', 'Middle of the Road', 'In and Output'],
+
+  set robotCapacity(newCapacity) {
+    if (typeof newCapacity === 'number') {
+      this._robotCapacity = newCapacity;
+    } else {
+      console.log(`Change ${newCapacity} to a number.`)
+    }
+  },
+  get robotCapacity() {
+    return this._robotCapacity;
+  }
+}
+
+class User {
+
+  constructor(name) {
+    // invokes the setter
+    this.name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    if (value.length < 4) {
+      alert("Name is too short.");
+      return;
+    }
+    this._name = value;
+  }
+
+}
+
+let user = new User("John");
+alert(user.name); // John
+
+user = new User(""); // Name is too short.
